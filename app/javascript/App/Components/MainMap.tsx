caac8f1e18cfc6f7befdd519dd698873b33d71d8
Joshua Hume <valencelectron@hotmail.com>
2021-05-05 11:01:48 +0900

    first commit

---
+++ app/javascript/App/Components/MainMap.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
+
+const MainMap = () => {
+  return <div className="MainMap"></div>;
+};
+export default MainMap;
---
+++ app/javascript/packs/Pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Home = () => {
+  return <h1>Japan Backroad Database</h1>;
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Home />, document.getElementById('root'));
+});
